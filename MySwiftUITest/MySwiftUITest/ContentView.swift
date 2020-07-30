09545090cd65ed9f3a5b1689d62b6612fe28d532
Gayathri <gayathri@idsoftsource.com>
2020-07-30 16:20:36 +0530

    Initial commit
    Initial commit

---
+++ MySwiftUITest/MySwiftUITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUITest
+//
+//  Created by Manimala Sundaram on 30/07/20.
+//  Copyright © 2020 IDSS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
